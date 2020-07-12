7a46eae34c0e02512b6356ac7b70c1c5f8a87190
ajfernandes15 <ajfernandes15@users.noreply.github.com>
2020-07-11 23:01:55 -0400

    Config del servidor

---
+++ src/index.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const morgan = require('morgan');
+const exphbs = require('express-handlebars');
+const path = require('path');
+
+//inicialitations
+
+const app = express();
+
+//settings
+app.set('port', process.env.PORT || 5000);
+app.set('views', path.join(__dirname, 'views'));
+app.engine('.hbs', exphbs({
+    defaultLayout: 'main',
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
+    extname: '.hbs',
+    helpers: require('./lib/handlebars')
+  }))
+  app.set('view engine', '.hbs');
+  app.use(express.urlencoded({extended: false}));//recibir los datos de usuarios 
+  app.use(express.json());
+//Middlewares
+
+app.use(morgan('dev'));
+
+//Global variables
+
+app.use((req, res, next) => {
+ 
+    next();
+
+  });
+
+//Routes
+app.use(require('./routes'));
+app.use(require('./routes/authentication'));
+app.use('/perfumes',require('./routes/perfumes'));
+
+//Public
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Starting the server
+
+app.listen(app.get('port'), () =>{
+    console.log('Server on port: ',app.get('port'));
+    
+    })
