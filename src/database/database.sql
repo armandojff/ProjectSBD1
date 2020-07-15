7a6c1d77f81d231e573f42e8382bddea3e9f8dd1
ajfernandes15 <ajfernandes15@users.noreply.github.com>
2020-07-15 16:42:23 -0400

    antes de pdfkit

--- src/database/database.sql
+++ src/database/database.sql
@@ -9,4 +9,15 @@ CREATE TABLE perfume(
 	presentacion int
 );
 
+INSERT INTO "perfume" values ("bleu de chanel",100);
 
+CREATE TABLE "tablaprueba" (
+  "id_prueba" integer not null,
+  "nombre_prueba" character varying not null,
+  "descripcion_prueba" character varying not null,
+ CONSTRAINT prueba_pkey PRIMARY KEY ("id_prueba")
+);
+
+INSERT INTO "Tabla_Prueba" values (1,'prueba1','esta prueba es la numero 1 para la entrega opcional');
+INSERT INTO "Tabla_Prueba" values (2,'prueba2','esta prueba es la numero 2 para la entrega opcional');
+INSERT INTO "Tabla_Prueba" values (3,'prueba3','esta prueba es la numero 3 para la entrega opcional');
