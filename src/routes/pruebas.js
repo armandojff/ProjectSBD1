2521e656006511b3db099e21bd443e3508fc5dc9
ajfernandes15 <ajfernandes15@users.noreply.github.com>
2020-07-15 20:02:27 -0400

    listo reporte

--- src/routes/pruebas.js
+++ src/routes/pruebas.js
@@ -1,7 +1,14 @@
 const express = require('express');
+
 const router = express.Router();
 
 const pool = require('../database');
+
+const fs = require("fs");
+
+const PDFDocument = require("../reportes/pdfkit-tables");
+
+const path = require('path');
 
 
 
@@ -65,6 +72,65 @@ router.get('/edit/:id', async (req,res) => {
 
 
  
+router.get('/reporte/:id', async (req,res) => {
 
+    const {id} = req.params;
+
+    var text = 'SELECT * FROM tablaprueba WHERE idprueba = $1';
+   
+    const value = [id];
+    
+    const results = await pool.query(text,value);
+
+    // Create The PDF document
+    const doc = new PDFDocument();
+
+    // Load the registers 
+    const registros = results.rows;
+
+    // Pipe the PDF into a patient's file
+
+    const dirreportes = path.join(__dirname, '../reportes/descargados');
+
+    //const dirlogo = path.join(__dirname,'../img/logo.png.jpg');
+
+    const nombrerep = registros[0].idprueba;
+
+    console.log(nombrerep);
+
+    doc.pipe(fs.createWriteStream(dirreportes + '/reporte-de-' + nombrerep + '.pdf'));
+
+    // Add the header - https://pspdfkit.com/blog/2019/generate-invoices-pdfkit-node/
+    doc
+    //.image("logo.png", 50, 45, { width: 50 })
+        //.image(dirlogo,50,45,{ with: 20} )
+        .fillColor("#444444")
+        .fontSize(20)
+        .text("Reporte de prueba.", 90, 57)
+        .fontSize(10)
+        .text("Datos para la tabla de id: "+registros[0].idprueba, 90, 90, { align: "center" })
+        //.text("Chamblee, GA 30341", 200, 80, { align: "right" })
+        .moveDown();
+
+    // Create the table - https://www.andronio.me/2017/09/02/pdfkit-tables/
+    const table = {
+        headers: ["Id", "Nombre", "Descripcion"],
+        rows: []
+    };
+
+        // Add the patients to the table
+    for (const registro of registros) {
+      table.rows.push([registro.idprueba, registro.nombreprueba, registro.descripcionprueba])
+    }
+
+    // Draw the table
+    doc.moveDown().table(table, 30, 125, { width: 590 });
+
+    // Finalize the PDF and end the stream
+    doc.end();
+
+    res.redirect('/pruebas');
+
+})
 
 module.exports = router;
